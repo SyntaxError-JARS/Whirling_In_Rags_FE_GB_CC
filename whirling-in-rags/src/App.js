c254d7d08d716fac1cc96e1401f9b92f609d1176
Catherine Claunch <catherine490@revature.net>
2022-06-01 15:29:31 -0500

    Added Customer Login and AddCreditCard

--- whirling-in-rags/src/App.js
+++ whirling-in-rags/src/App.js
@@ -1,4 +1,5 @@
 import DeleteCreditCard from "./components/CreditCard/DeleteCreditCard";
+import CustomerLogIn from "./components/Customer/CustomerLogIn";
 import CustomerRegister from "./components/Customer/CustomerRegister";
 import CreateMenuItem from "./components/Menu/CreateMenuItem";
 import MenuItem from "./components/Menu/MenuItem";
@@ -8,6 +9,7 @@ import ViewOrders from "./components/Order/ViewOrders";
 function App() {
   return (
     <>
+    <CustomerLogIn />
     <CustomerRegister />
     <br></br>
     <CreateMenuItem />
---
+++ whirling-in-rags/src/components/Customer/CustomerLogIn.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useRef } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { userContext } from "../../App";
+
+export default function CustomerLogIn() {
+    const usernameInput = useRef();
+    const passwordInput = useRef();
+    // const [user, setUser] = useContext(userContext);
+    // const navigate = useNavigate();
+    const url = "https://whrilinginrags.azurewebsites.net";
+
+    async function login() {
+        
+        const customer = {
+            username: usernameInput.current.value,
+            password: passwordInput.current.value,
+        };
+
+        if (customer.password === "hello") {
+            alert("You need to reset password");
+        } else {
+            try {
+                const response = await axios.post(`${url}/auth`, customer);
+                console.log(response.data);
+                // console.log("Hey this is the user prior ", user);
+                // setUser({ ...user, username: customer.username });
+                // console.log("This is after we set the user ", user);
+                // the below code, manipulates the DOM
+                // window.location.replace("http://localhost:3000/dashboard");
+                // navigate("/dashboard");
+            } catch (error) {
+                console.error(error.response.data);
+                alert(error.response.data);
+            }
+        }
+    }
+
+    return (
+        <>
+            <h4>Welcome back, please log in below.</h4>
+            <input placeholder="Enter username" ref={usernameInput}></input>
+            <input type="password" placeholder="Enter password" ref={passwordInput}></input>
+            <button onClick={login}>Login</button>
+        </>
+    );
+}
