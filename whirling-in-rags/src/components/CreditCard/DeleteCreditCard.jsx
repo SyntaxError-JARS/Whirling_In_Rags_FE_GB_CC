514b151ddcf3d68ee23b9d149473b53a2d93fe6f
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-02 14:21:20 -0500

    can now delete a creadit card in react.

--- whirling-in-rags/src/components/CreditCard/DeleteCreditCard.jsx
+++ whirling-in-rags/src/components/CreditCard/DeleteCreditCard.jsx
@@ -12,14 +12,8 @@ export default function DeleteCreditCard(){
 
     async function deleteCreditCard(){
 
-        const creditCardIn = {
-            cardNumber: creditCardInput.current.value
-        }
-
-
         try{
-            const encodedValue = encodeURIComponent(creditCardIn);
-            const response = await axios.delete((`${url}/Credit/delete?cardNumber=${encodedValue}` , creditCardIn ))
+            const response = await axios.delete(`${url}/Credit/delete-card?cardNumber=${creditCardInput.current.value}` )
             console.log(response)
             console.log(response.data)
         }catch (error){
@@ -30,11 +24,11 @@ export default function DeleteCreditCard(){
 
     return(
         <>
-          <h3>Are you sure you want to delete your creditcard? If so please enter your username and hit the delete credit card button.</h3>
+          <h3>Are you sure you want to delete your creditcard? If so please enter your Credit Card Number and hit the delete credit card button.</h3>
         <br></br>
         <br></br>
         <br></br>
-        <input placeholder="Enter your username" ref={creditCardInput}></input>
+        <input placeholder="Enter your Credit Card Number" ref={creditCardInput}></input>
         <br></br>
         <button onClick={deleteCreditCard}>Delete CreditCard</button>
         </>
