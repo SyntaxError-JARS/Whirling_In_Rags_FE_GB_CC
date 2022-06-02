6c782eeeba3412843b7ab86ed1e03427309b04f2
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-02 14:47:38 -0500

    On Credit card delete a message pops ups that the customer deleted there card.

--- whirling-in-rags/src/components/CreditCard/DeleteCreditCard.jsx
+++ whirling-in-rags/src/components/CreditCard/DeleteCreditCard.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function DeleteCreditCard(){
 
+    const [showDelete, setShowDeleted] = useState(false);
 
     const creditCardInput = useRef();
 
@@ -22,6 +23,7 @@ export default function DeleteCreditCard(){
 
     }
 
+
     return(
         <>
           <h3>Are you sure you want to delete your creditcard? If so please enter your Credit Card Number and hit the delete credit card button.</h3>
@@ -30,7 +32,12 @@ export default function DeleteCreditCard(){
         <br></br>
         <input placeholder="Enter your Credit Card Number" ref={creditCardInput}></input>
         <br></br>
-        <button onClick={deleteCreditCard}>Delete CreditCard</button>
+        <button onClick={() => { 
+            deleteCreditCard(); 
+            setShowDeleted(!showDelete) 
+            }}
+            >Delete CreditCard</button>
+        {showDelete && <p>You Have Successfully Deleted Your Credit Card</p>}
         </>
 
     )
