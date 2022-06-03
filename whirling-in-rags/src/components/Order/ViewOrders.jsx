456621b1c39128e0d0a12c93e3849a5558a02c6a
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-03 10:37:28 -0500

    created a way for a user to input their username to find there orders. not the most intuative solution but a working solution.

--- whirling-in-rags/src/components/Order/ViewOrders.jsx
+++ whirling-in-rags/src/components/Order/ViewOrders.jsx
@@ -1,25 +1,34 @@
 import axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CustomerNavBar } from "../Customer/CustomerNavBar";
 
 import './OrderTable.css';
 
 export default function ViewOrders(){
     const [orderBody, setOrderBody] = useState();
+    const [showMessage, setShowMessage] = useState(false);
 
 
-    
+    const usernameInput = useRef();
 
 
     const url = "https://whrilinginrags.azurewebsites.net"
 
-
+    // if(usernameInput === null ){}
     async function viewAllOrders(){
 
+        const usernameinput = {
+            username: usernameInput.current.value 
+        }
+        if(usernameInput.current.value === ""){
+            setShowMessage(!showMessage)
+        }else{
         try{
-        const response = await axios.get(`${url}/order/whirling-in-rangs-orders`)
-        const orders = await response.data;
-        const menuItemsTableRows = orders.map((e) => {
+            console.log(usernameInput)
+            const encodedPara = encodeURIComponent(usernameinput)
+            const response = await axios.get(`${url}/order/whirling-in-rangs-orders?=username${encodedPara}`)
+            const orders = await response.data;
+            const menuItemsTableRows = orders.map((e) => {
             return (
                 <tr>
                     <td>{e.id}</td>
@@ -32,22 +41,26 @@ export default function ViewOrders(){
             )
            
         })
+    
         setOrderBody(menuItemsTableRows)
         console.log(response)
         console.log(response.data)
+    
     } catch (e) {
         console.error(e)
     }
     }
+}
+
 
     return(
         <>
      <CustomerNavBar />
         <h3>Welcome to the Orders Page</h3>
         
-        
-        <button onClick={viewAllOrders}>Press to Find All of your Orders</button>
-    
+        <input placeholder="Input your Username" ref={usernameInput}></input>
+        <button onClick={ viewAllOrders }>Press to Find All of your Orders</button>
+        {showMessage && <p>No username was detected please input your username and try again</p>}
         <table>
             <thead>
                 <tr>
